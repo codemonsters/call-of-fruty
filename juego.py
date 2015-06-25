0838ee463bad4169bda83c8712acefe002b1e46c
12-trunks-12 <angel.miguelez.edu@gmail.com>
2015-06-25 17:31:04 +0200

    Añadido menú con logo

--- juego.py
+++ juego.py
@@ -12,6 +12,7 @@ pygame.init()
 
 def jugar(surface, fps_clock):
     imagen_fondo = pygame.image.load("imagenes/imagen_fondo.jpg")
+    imagen_jugador = pygame.image.load("imagenes/jugador_sprite.png")
     while True: # main game loop
         for event in pygame.event.get():
             if event.type == pygame.locals.QUIT:
@@ -19,7 +20,9 @@ def jugar(surface, fps_clock):
                 sys.exit()
 
         surface.blit(imagen_fondo, (0, 0))
-
+        surface.blit(imagen_jugador, (300, 500))
+        #pygame.draw.rect(surface, pygame.Color(255, 0, 0), pygame.Rect(360, 100, 20, 20))  # Rojo
+        #pygame.draw.rect(surface, pygame.Color(255, 0, 0), pygame.Rect(385, 100, 30, 50))  # Rojo
 
         #------------------Dibujar, FPS
         pygame.display.update()
